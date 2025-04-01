f2a5542fc0612641679623e36028bef32bb9228d
郝一平 <14609765+hao-yiping@user.noreply.gitee.com>
2025-04-01 16:56:48 +0800

    template .bug

--- src/Symbolic/expression.cpp
+++ src/Symbolic/expression.cpp
@@ -2710,29 +2710,29 @@ void Expres::differetial(size_t X1, size_t X2, bool Input)
     {
         if (output[i] != NULL) queue.append(output[i]);
     }
-    printf("?????\n");
+    //printf("?????\n");
     formula.BFTbackward(valid, queue);
-    printf("BFTbackward\n");
+    //printf("BFTbackward\n");
     formula.TopoSortBFS(sequence);
-    printf("TopoSortBFS\n");
+    //printf("TopoSortBFS\n");
     formula.Shrink(valid, sequence);
-    printf("?????\n");
+    //printf("?????\n");
 
 
     length = sequence.count();
     label.recount(formula.count());
     label.value(NULL);
-    printf("?????\n");
-    for (i = 0; i < length; i++)
-    {
-        here = sequence[i];
-        printf("\t%zu\n", here->site());
-    }
+    //printf("?????\n");
+    //for (i = 0; i < length; i++)
+    //{
+        //here = sequence[i];
+        //printf("\t%zu\n", here->site());
+    //}
     for (i = 0; i < length; i++)
     {
         here = sequence[i];
         site = here->site();
-        printf("\t%zu\n", here->site());
+        //printf("\t%zu\n", here->site());
         switch (here->Type)
         {
         case _LeafX_:
@@ -2770,7 +2770,7 @@ void Expres::differetial(size_t X1, size_t X2, bool Input)
         output[i] = label[output[i]->site()];
         formula[output[i]->site()]->Output = true;
     }
-    printf("?????\n");
+    //printf("?????\n");
 
 }
 void Expres::ParameterBackward(size_t No)
