9945e635d273dc8a9167fe8cc3d419a37a247cad
郝一平 <14609765+hao-yiping@user.noreply.gitee.com>
2025-04-07 15:47:39 +0800

    exp parser test

--- src/Symbolic/expression.cpp
+++ src/Symbolic/expression.cpp
@@ -2963,6 +2963,7 @@ void Expres::differetial(size_t X1, size_t X2, bool Input)
         printf("\toutput[i]:%zu\n", (size_t)output[i]);
         printf("\toutput[i]->site():%zu\n", output[i]->site());
         formula[output[i]->site()]->Output = false;
+        printf("\t\t?????\n");
         output[i] = label[output[i]->site()];
         printf("\toutput[i]->site():%zu\n", output[i]->site());
         formula[output[i]->site()]->Output = true;
