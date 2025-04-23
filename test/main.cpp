08053a4a74c8c7e5b199e595a2682b09cdc68f98
郝一平 <14609765+hao-yiping@user.noreply.gitee.com>
2025-04-23 20:21:35 +0800

    test.cpp add

--- test/main.cpp
+++ test/main.cpp
@@ -22,24 +22,7 @@ int main()
     return error;
 }
 
-class CFile
-{
-public:
-    CFile();
-    ~CFile();
-    void close(void);
-    void Open(const char* name, const char* mode);
-    void OpenRead(const char* s);//SH_read_from_file__(const char* s);
-    void OpenWriteAtRear(const char* s);//fp_write_at_rear(const char* s);
-    void OpenWritePlus(const char* s);//fp_write_plus
-    std::string FusePathAndName(const std::string& path, const std::string& name);//path_add_file_name
-    std::string FusePathAndName(const char* path, const std::string& name);//path_add_file_name
-    std::string ChangeSuffix(const std::string& file, const char* new_one);//string suffix_change(const string& file, const char* new_one);
-    
-    FILE* fp;
-private:
 
-};
 
 
 int static test01(const parameter& para);
