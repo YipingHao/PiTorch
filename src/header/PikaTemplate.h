90164e2887d58f125c2e55d972711c1963e443a1
郝一平 <14609765+hao-yiping@user.noreply.gitee.com>
2025-04-24 17:23:52 +0800

    CRTP

--- src/header/PikaTemplate.h
+++ src/header/PikaTemplate.h
@@ -1278,7 +1278,8 @@ namespace Pikachu
 		}
 		catch (hyperlex::dictionary* error)
 		{
-			error->append("loaction", "graph<V>::lift");
+			printf("11111???\n");
+			//error->append("loaction", "graph<V>::lift");
 			throw error;
 		}
 		target->out.clear();
