adcd635cae84e3cb5322eaea472d1f674b9e7cf5
郝一平 <14609765+hao-yiping@user.noreply.gitee.com>
2025-04-24 16:48:29 +0800

    CRTP

--- src/Symbolic/expression.cpp
+++ src/Symbolic/expression.cpp
@@ -4041,9 +4041,9 @@ bool Expres::Simplify08(void)
             break;
         }
         if ((operation)here->Code == _div_)
-        if(false)
+        //if(false)
         {
-            //std::cout << "Here: " << i << std::endl;
+            std::cout << "Here: " << i << std::endl;
             for (j = 0; j < 4; j++)
             {
                 LL = j / 2;
