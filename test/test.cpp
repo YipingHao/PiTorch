74c318ae267570f2da108301c9d70a1b500cb528
郝一平 <14609765+hao-yiping@user.noreply.gitee.com>
2025-07-07 21:48:39 +0800

    parser.*  compile and test string pool

--- test/test.cpp
+++ test/test.cpp
@@ -752,7 +752,7 @@ namespace hyperlex
 			size_t hash = 5381;
 			int c;
 			while ((c = *str++)) {
-				hash = ((hash << 5) + hash) ^ c;
+				hash = ((hash << 4) + hash) ^ c;
 				//hash = (hash >> 16) ^ (hash & 0xFFFF); // 混合高位与低位[2,6](@ref)
 			}
 			return hash;
