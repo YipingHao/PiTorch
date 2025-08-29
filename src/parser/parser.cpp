b54a2734a7624450520a7766ebdac79b97bcdc54
郝一平 <14609765+hao-yiping@user.noreply.gitee.com>
2025-08-29 16:03:02 +0800

    print name and label

--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -1783,7 +1783,7 @@ int BuildInfor::buildNETCheck(const lex& eme, GTNode* NETBODY, context* dst, Net
 		{
 			for (size_t j = 0; j < temp->count(); j++)
 			{
-				net->AppendOutput(temp->GetTensor(j));
+				net->output.append(temp->GetTensor(j));
 			}
 			for (size_t j = 0; j < net->output.count(); j++)
 			{
